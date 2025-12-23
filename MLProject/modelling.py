8a4f043af463b7b23665b0c72a30084d725ea457
Nida Rofi'ah <168633040+stawberriespie@users.noreply.github.com>
2025-12-23 09:37:32 +0700

    Refactor modelling.py for MLflow integration
    Updated MLflow tracking URI and added context for data loading and model training.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,26 +1,27 @@
-import pandas as pd
 import mlflow
-import mlflow.sklearn
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("penguins_preprocessing.csv")
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_experiment("Penguins_Classification")
 
-X = df.drop(columns=["species"])
-y = df["species"]
+# Load data
+data = pd.read_csv("penguins_preprocessing.csv")
+
+X = data.drop("species", axis=1)
+y = data["species"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.set_experiment("Penguins_Classification")
-mlflow.sklearn.autolog()
+# autolog
+mlflow.autolog()
 
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-
-print("Accuracy:", acc)
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
